7614c2f23b742f63f9c6d6ad686f86382871b2c0
Michal Piksa <Michal.Piksa@embedit.com>
2025-02-12 16:32:20 +0100

    First commit

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,10 +1,51 @@
 ﻿namespace Calculator
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CalcAddition(3, 2);
+
+            CalcSubtraction(3, 2);
+
+            CalcMultiplication(3, 2);
+
+            CalcDivide(3, 0);
+
         }
+        public static void CalcAddition(double number1, double number2)
+        {
+            double result = number1 + number2;
+            Console.WriteLine($"Result is {result}");
+        }
+
+        public static void CalcSubtraction(double number1, double number2)
+        {
+            double result = number1 - number2;
+            Console.WriteLine($"Result is {result}");
+        }
+
+        public static void CalcMultiplication(double number1, double number2)
+        {
+            double result = number1 * number2;
+            Console.WriteLine($"Result is {result}");
+        }
+
+        public static void CalcDivide(double number1, double number2)
+        {
+            try
+            {
+                double result = number1 / number2;
+                Console.WriteLine($"Result is {result}");
+
+            }
+            catch (Exception)
+            {
+
+                throw new DivideByZeroException();
+            }
+        }
+
+
     }
 }
