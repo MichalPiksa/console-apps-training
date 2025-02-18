808623fe218aacb8599328fbf59a0a17de87a8f4
Michal Piksa <Michal.Piksa@embedit.com>
2025-02-18 13:21:08 +0100

    Added Palindrome Checker game and FizzBuzz

---
+++ Calculator/FizzBuzzVersion2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace FizzBuzzVersion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome in FizzBuzz game!");
+
+            while (true)
+            {
+                Console.WriteLine("Enter a Fizz replacement:");
+                var fizzReplacement = Console.ReadLine();
+                Console.WriteLine("Enter a Buzz replacement:");
+                var buzzReplacement = Console.ReadLine();
+
+                var divisors = ValidDivisorsInput();
+                foreach (var divisor in divisors)
+                {
+                    Console.WriteLine(divisor);
+                }
+            }
+        }
+
+        public static List<int> ValidDivisorsInput()
+        {
+            List<int> validListOfDivisors = new List<int>();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter at least 3 whole number divisors separated by '_'");
+                string inputDivisors = Console.ReadLine();
+                var listOfDivisors = inputDivisors.Split("_");
+                if (listOfDivisors.Length < 3)
+                {
+                    Console.WriteLine("Invalid input. Try it again!");
+                    continue;
+                }
+                foreach (var item in listOfDivisors)
+                {
+                    if (int.TryParse(item, out int numberItem))
+                    {
+                        validListOfDivisors.Add(numberItem);
+                        flag = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Try it again!");
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            return validListOfDivisors;
+        }
+    }
+}
---
+++ Calculator/PalindromeChecker/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PalindromeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome in Palindrom checker game!");
+            while (true) 
+            {
+                Console.WriteLine("\nWrite a word and I will tell you, if it is Palindrome");
+                Console.WriteLine("For exit type 'q'");
+                string? userInput = Console.ReadLine();
+                if (userInput == "q")
+                {
+                    break;
+                }
+                
+                while (string.IsNullOrWhiteSpace(userInput) || userInput.Length < 3 || !userInput.All(char.IsAsciiLetter))
+                {
+                    Console.WriteLine("(Input must be at least 3 characters long and contain only letters)");
+                    userInput = Console.ReadLine();
+                }
+
+                string reversedInput = string.Empty;
+                for (int i = userInput.Length - 1; i >= 0; i--)
+                { 
+                    reversedInput = reversedInput + userInput[i];
+                }
+
+                if (userInput.ToLower() == reversedInput.ToLower())
+                {
+                    Console.WriteLine($"Yes, word \"{userInput}\" is Palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine($"No, word \"{userInput}\" is not Palindrome. \"{userInput}\" and \"{reversedInput}\" are not equal.");
+                }
+            }
+            Console.WriteLine("Game exit.");
+        }
+    }
+}
