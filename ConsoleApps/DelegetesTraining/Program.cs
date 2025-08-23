d25862854e13552ea5f3354254b841ebac4a3e41
Majkl <mpiksa@seznam.cz>
2025-08-23 15:46:08 +0200

    Added FindMaximumNumber and ReverseArray methods

---
+++ ConsoleApps/DelegetesTraining/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DelegetesTraining;
+
+class Program
+{
+    delegate void PrintText(string text, DateTime dateTime);
+    static void Main(string[] args)
+    {
+        PrintText result = new PrintText(PrintTextToScreen);
+        
+        Console.WriteLine("Enter your name: ");
+        string userInput = Console.ReadLine();
+        
+        result(userInput, DateTime.Now);
+
+        Console.ReadKey();
+    }
+
+    static void PrintTextToScreen(string text, DateTime dateTime)
+    {
+        Console.WriteLine($"{dateTime}, {Guid.NewGuid()}: {text}");
+    }
+}
