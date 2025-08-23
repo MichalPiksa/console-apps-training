617ec918b2cf20fd855625ba4c8e97a50690e7ab
Majkl <mpiksa@seznam.cz>
2025-08-23 16:19:51 +0200

    Added second delegate

--- ConsoleApps/DelegetesTraining/Program.cs
+++ ConsoleApps/DelegetesTraining/Program.cs
@@ -5,7 +5,9 @@ class Program
     delegate void PrintText(string text, DateTime dateTime);
     static void Main(string[] args)
     {
-        PrintText result = new PrintText(PrintTextToScreen);
+        Log log = new Log();
+        //PrintText result = new PrintText(PrintTextToScreen);}
+        PrintText result = new PrintText(log.PrintTextToFile);
         
         Console.WriteLine("Enter your name: ");
         string userInput = Console.ReadLine();
@@ -14,9 +16,20 @@ class Program
 
         Console.ReadKey();
     }
+}
 
-    static void PrintTextToScreen(string text, DateTime dateTime)
+public class Log
+{
+    public void PrintTextToScreen(string text, DateTime dateTime)
     {
         Console.WriteLine($"{dateTime}, {Guid.NewGuid()}: {text}");
+    }
+
+    public void PrintTextToFile(string text, DateTime dateTime)
+    {
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+        {
+            sw.WriteLine($"{dateTime}, {Guid.NewGuid()}: {text}");
+        }
     }
 }
